eedb2813c34bd06071fc19adbe6fb06bb4dee700
TaysonGermano <taysongermano@gmail.com>
2022-02-22 13:40:27 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Card, AdviceID, Button } from "./Card";
+import divider from "./pattern-divider-desktop.svg";
+import dice from "./icon-dice.svg";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  async function getAdvice() {
+    const advice = await axios("https://api.adviceslip.com/advice");
+    const response = advice.data;
+    setAdvice(response.slip);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card>
+      <AdviceID>Advice #{advice.id}</AdviceID>"{advice.advice}"
+      <img src={divider} alt="qoute" width="100%" />
+      <Button onClick={getAdvice}>
+        <img src={dice} alt="qoute" width="20px" />
+      </Button>
+    </Card>
   );
 }
 
